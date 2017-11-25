40457d69aeb847106cca58ae80feb5360efdc842
AllyJo <gatorally245@gmail.com>
2017-11-24 23:59:51 -0800

    correct path link for cat#show to show films per cat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'films#index'
+  get '/categories/:category_id/films', to: 'films#category_index', as: 'category_index'
 
   resources :users, only: [:new, :create, :show]
 
