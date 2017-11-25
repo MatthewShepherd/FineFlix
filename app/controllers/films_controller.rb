484b7f8528382f007885cc328410cf91725c96b2
AllyJo <gatorally245@gmail.com>
2017-11-24 20:11:52 -0800

    modify routes for films controller

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -2,8 +2,19 @@ class FilmsController < ApplicationController
 
   def index
     # @category = Category.find(params[:category_id])
+    # @films = Film.find(params[:id])
+  # if @films.nil?
     @films = Film.all
+  #   render "index"
+    # end
+  end
+
+  def show
+    @film = Film.find(params[:id])
+    @category = @film.category
   end
 
 
 end
+
+
