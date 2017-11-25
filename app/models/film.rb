2edbd1c32c0019f98aad4c19e18f5a55777129fa
AllyJo <gatorally245@gmail.com>
2017-11-24 23:37:58 -0800

    delete unused code

--- app/models/film.rb
+++ app/models/film.rb
@@ -7,18 +7,7 @@ class Film < ApplicationRecord
   def average_rating
     self.ratings.reduce(0) {|sum, rating| sum += rating.value} / self.ratings.size
   end
-  def film_comments
-    self.comments.each do |comment|
-      print comment.body
-    end
-  end
 
-  def film_reviews
-    self.reviews.each do |review|
-      p review.title
-      p review.body
-    end
-  end
 end
 
 
