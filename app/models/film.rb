7734bccc31a318dee5545c7271620d278d4506b9
AllyJo <gatorally245@gmail.com>
2017-11-24 20:12:22 -0800

    add method to get average rating per film

--- app/models/film.rb
+++ app/models/film.rb
@@ -3,4 +3,22 @@ class Film < ApplicationRecord
   has_many :ratings, as: :rateable
   has_many :reviews
   has_many :comments, as: :commentable
+
+  def average_rating
+    self.ratings.reduce(0) {|sum, rating| sum += rating.value} / self.ratings.size
+  end
+  def film_comments
+    self.comments.each do |comment|
+      print comment.body
+    end
+  end
+
+  def film_reviews
+    self.reviews.each do |review|
+      p review.title
+      p review.body
+    end
+  end
 end
+
+
