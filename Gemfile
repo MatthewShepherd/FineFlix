4da4179aa999984426835a5c8300297f46f193ff
AllyJo <gatorally245@gmail.com>
2017-11-25 11:58:08 -0800

    add gems and methods to get film posters from api

--- Gemfile
+++ Gemfile
@@ -30,6 +30,10 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'httparty'
+gem 'extra_print'
+gem 'better_errors'
+gem 'binding_of_caller'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/film.rb
+++ app/models/film.rb
@@ -4,8 +4,21 @@ class Film < ApplicationRecord
   has_many :reviews
   has_many :comments, as: :commentable
 
+  after_create :fetch_images
+
+
   def average_rating
     self.ratings.reduce(0) {|sum, rating| sum += rating.value} / self.ratings.size
+  end
+
+  def fetch_images
+    @adapter = ImdbAdapter.new
+    eap self.title
+    images = @adapter.search_by_title(self.title)
+    return nil if images.nil?
+    self.image_url = images['large']
+    self.thumbnail_url = images['thumb']
+    self.save
   end
 
 end
